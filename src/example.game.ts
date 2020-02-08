9b383bd38b69bc11a592d7fa4c31948a67c936d7
hscheuerle <h.a.scheuerle@gmail.com>
2020-02-08 18:43:39 -0500

    1 file modified

--- src/example.game.ts
+++ src/example.game.ts
@@ -1,10 +1,13 @@
 import * as Phaser from 'phaser';
+import { DataManagerScene } from './data-manager.scene';
 
-const gameConfig: Phaser.Types.Core.GameConfig = {
+const gameConfig = {
   height: 700,
   width: 350,
+  pixelArt: true,
   backgroundColor: '#333333',
   parent: 'app',
+  scene: [ DataManagerScene ],
 };
 
 export class ExampleGame extends Phaser.Game {
