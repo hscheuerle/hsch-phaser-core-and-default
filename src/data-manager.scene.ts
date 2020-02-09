bf3fdddae87b4fdfccb9edcb6ab80226260528ff
hscheuerle <h.a.scheuerle@gmail.com>
2020-02-08 20:50:54 -0500

    1 file modified

--- src/data-manager.scene.ts
+++ src/data-manager.scene.ts
@@ -15,19 +15,21 @@ export class DataManagerScene extends Phaser.Scene {
   }
 
   preload() {
+    this.data.events.on('changedata', (...data) => this.onChangeData(data));
+    this.data.events.on('setdata', (...data) => this.onChangeData(data));
     this.data.set('count', 0);
-    // this.data.set({ count: 0, test: 2 }); why isn't this working in intellisense?
+    this.data.set('bonus', 5);
   }
 
   create() {
     this.countText = this.add.text(10,10, 'count');
     this.countText.setDataEnabled();
-    this.data.events.on('changedata', (...data) => this.onChangeData(data));
+    this.countText.on('changedata', (...data) => this.onChangeData(data))
 
     this.buttonRect = this.add.rectangle(20, 100, 120, 80, 0x0000a2);
     this.buttonRect.setInteractive();
+    // remember to use closure, bind this, or pass 'this' as 3rd param
     this.buttonRect.on('pointerup', (pointer) => this.onButtonClick(pointer)); 
-    // bind this or put in closure!
   }
 
   update(timer, delta) {
@@ -39,11 +41,17 @@ export class DataManagerScene extends Phaser.Scene {
   onButtonClick(pointer) {
     this.buttonRect.fillColor = 0x0000b2;
     const lastCount = this.data.get('count') | 0;
+    const lastBonus = this.data.get('bonus') | 0;
     this.data.set('count', lastCount + 1);
+    this.data.set('bonus', lastBonus + 1);
+    // setData same as data.set???
+    this.countText.setData('count2', 2 * (lastCount + 1));
+    // even with this typing being broken, this just calls set for each key triggering the callback multiple times
+    // this.data.set({'count': lastCount + 1, 'bonus': lastCount + 5}); 
   }
 
   onChangeData(data) {
-    
-    console.log('on change data', data[1], data[2], data[3]);
+    // called 
+    console.log(Object.keys(data[0]), data[1], data[2], data[3]);
   }
 }
